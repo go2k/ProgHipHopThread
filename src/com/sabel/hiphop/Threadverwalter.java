f87fa607f0baa4a900a309451b7c572366fcccfe
Michael König <m.koenig@go2k.de>
2017-12-20 10:50:14 +0100

    initialer commit

--- src/com/sabel/hiphop/Threadverwalter.java
+++ src/com/sabel/hiphop/Threadverwalter.java
@@ -33,7 +33,6 @@ public class Threadverwalter implements Runnable {
         }
         System.out.println("thread beendet");
     }
-
     public static void main(String[] args) throws IOException {
 
         Threadverwalter threadverwalter = new Threadverwalter("");
